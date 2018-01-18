adf9d321668d92afd52d8e3f023eb3ae0177c44d
204Constie <kat.bana204@gmail.com>
2018-01-18 23:15:49 +0100

    init commit

---
+++ Experiment.h
@@ -0,0 +1,36 @@
+/*
+ * Experiment.h
+ *
+ *  Created on: 16 maj 2016
+ *      Author: oramus
+ */
+
+#ifndef EXPERIMENT_H_
+#define EXPERIMENT_H_
+
+#include "Result.h"
+
+class Experiment {
+private:
+	long *histogram; // histogram
+	bool *used; // wektor pomocniczy przy losowaniu bez zwracania
+
+	bool *usedPerThread;
+
+	int drawsNumber; // liczba kulek
+	int balls; // liczba kulek
+	long hmax; // maksymalny indeks histogramu
+	long hmin; // najmniejszy, rozsadny indeks histogramu
+
+	void clearUsed(); // kasuje pamiec o wylosowanych kulach
+	long singleExperimentResult();
+
+public:
+	Experiment(int balls, int drawsNumber);
+	virtual ~Experiment();
+
+	Result *calc(long experiments);
+
+};
+
+#endif /* EXPERIMENT_H_ */
