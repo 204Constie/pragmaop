c60df3208a99cb4448ae89f4d806de1be00dbf63
204Constie <kat.bana204@gmail.com>
2018-01-19 13:24:16 +0100

    scnd commit

--- Experiment.cpp
+++ Experiment.cpp
@@ -130,7 +130,7 @@ Result * Experiment::calc(long experiments) {
 		histogram[i]++;
 	}
 
-	cout << "--------------------------------------------------------" << endl;
+	// cout << "--------------------------------------------------------" << endl;
 	// cout << "hmin: " << hmin << " hmax: " << hmax << endl;
 	// cout << "hmin: " << histogram[hmin] << " hmax: " << histogram[hmax] << endl;
 
@@ -147,7 +147,7 @@ Result * Experiment::calc(long experiments) {
 {
 #pragma omp for reduction(+: sum, values)
 	for (long idx = hmin; idx <= hmax; idx++) {
-		cout << "idx: " << idx << " histogram[idx]: " << histogram[idx] << endl;
+		// cout << "idx: " << idx << " histogram[idx]: " << histogram[idx] << endl;
 		if (maxN < histogram[idx]) {
 			maxN = histogram[idx];
 			maxID = idx;
@@ -156,7 +156,7 @@ Result * Experiment::calc(long experiments) {
 		}
 		sum += idx * histogram[idx];
 		values += histogram[idx];
-		cout << "sum: " << sum << " values: " << values << endl;
+		// cout << "sum: " << sum << " values: " << values << endl;
 	}//for
 	#pragma omp critical
 	{
