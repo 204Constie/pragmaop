642366a51e15b084a4fded93d8a01484c6b82bda
204Constie <kat.bana204@gmail.com>
2018-01-19 01:45:09 +0100

    init commit

--- Experiment.cpp
+++ Experiment.cpp
@@ -108,7 +108,8 @@ long Experiment::singleExperimentResult() {
     // cout << "12 "  << endl;
 
 		// cout << "result: " << result << endl;
-		cout << "balls: " << ((double) balls * result) / ( RAND_MAX + 1.0)) << endl;
+		double hh = ((double) balls * result) / ( RAND_MAX + 1.0));
+		cout << "balls: " << hh << endl;
 		ball = 1 + (int) (((double) balls * result) / ( RAND_MAX + 1.0)); // rand losuje od 0 do RAND_MAX wlacznie
 
 		if (usedPerThread[ball - 1])
