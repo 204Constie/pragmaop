ca78ef28f5db6ea928baef020349cddc5b8cd666
204Constie <kat.bana204@gmail.com>
2018-01-19 02:24:17 +0100

    init commit

--- Experiment.cpp
+++ Experiment.cpp
@@ -43,37 +43,27 @@ Experiment::Experiment(int balls, int drawsNumber) {
 	cout << "Histogram min: " << hmin << " max: " << hmax << endl;
 
 	histogram = new long[hmax + 1];
-  // cout << "1 "  << endl;
+
 // each thread own one used array
-	// used = new bool[balls];
 	usedPerThread = new bool[balls];
 	bool *usedPerThread = usedPerThread;
-  // cout << "2 "  << endl;
 
 	for (long i = 0; i < hmax + 1; i++)
 		histogram[i] = 0;
-    // cout << "3 " << endl;
 }
 
 void Experiment::clearUsed() {
 
 	for (int i = 0; i < balls; i++)
 		usedPerThread[i] = false;
-		// used[i] = false;
-    // cout << "4 "  << endl;
 }
 
 long Experiment::singleExperimentResult() {
 	long sum = 0;
 	int ball;
 	double p;
-  // cout << "5 "  << endl;
 
 	clearUsed();
-
-  // cout << "6 "  << endl;
-
-	// int seed = (unsigned)(random() * (omp_get_thread_num()+2));
 
 	// struct plantSeed{
 	// 	plantSeed(bool b){
@@ -87,39 +77,26 @@ long Experiment::singleExperimentResult() {
 {
 	if(flag == 0){
 		flag = 1;
-		int seed = (unsigned)(random() * (omp_get_thread_num()+2));
-		// struct timeval tf;
-		// gettimeofday(&tf, NULL);
-		// int seed = tf.tv_sec *1000 + tf.tv_usec * 0.001 + omp_get_thread_num();
+		// int seed = (unsigned)(random() * (omp_get_thread_num()+2));
+		struct timeval tf;
+		gettimeofday(&tf, NULL);
+		int seed = tf.tv_sec *1000 + tf.tv_usec * 0.001 + omp_get_thread_num();
 		cout << "seed: " << seed << endl;
 		srand48_r(seed, &drand_Buffor);
 	}
 }
 
-  // cout << "7 "  << endl;
-	// srand48_r(seed, &drand_Buffor);
-// #pragma omp for private(ps)
-// #pragma omp for
 	for (int i = 0; i < drawsNumber; i++) {
-    // cout << "11 "  << endl;
-		double result = 0;
-		// drand48_r(drand_Buffor, &result);
-		drand48_r(&drand_Buffor, &result);
-    // cout << "12 "  << endl;
 
-		// cout << "result: " << result << endl;
-		double hh = ((double) balls * result) / ( 1 + 1.0);
-		int jj = (int) hh;
-		// cout << "result: " << jj << endl;
-		// cout << "balls: " << jj << endl;
+		double result = 0;
+		drand48_r(&drand_Buffor, &result);
+
 		ball = 1 + (int) (((double) balls * result ) / ( 1 + 1.0)); // rand losuje od 0 do RAND_MAX wlacznie
 
 		if (usedPerThread[ball - 1])
 			continue;
 
-    // cout << "21 "  << endl;
 		p = Distribution::getProbability(i + 1, ball); // pobieramy prawdopodobienstwo wylosowania tej kuli
-    // cout << "22 "  << endl;
 
 		if ((result / ( 1 + 1.0)) < p) // akceptacja wyboru kuli z zadanym prawdopodobienstwem
 				{
@@ -130,10 +107,9 @@ long Experiment::singleExperimentResult() {
 			sum += ball; // kule maja numery od 1 do balls wlacznie
 			i++;
 		}
-		// cout << "6666666666666" << endl;
 	}
 
-	// cout << "Suma = " << sum << endl;
+	cout << "Suma = " << sum << endl;
 
 	return sum;
 }
@@ -141,21 +117,13 @@ long Experiment::singleExperimentResult() {
 Result * Experiment::calc(long experiments) {
 	cout << "calc" << endl;
 
-// #pragma omp parallel
-// {
- // for shared(experiments, histogram) private(l)\
-				// reduction(+ : histogram)
 #pragma omp parallel for
 	for (long l = 0; l < experiments; l++) {
-		// cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
 		// i = singleExperimentResult() i pragma omp atomic zeby zabezpieczyc histogram
 		int i = singleExperimentResult();
-		// cout << "i: " << i << endl;
 #pragma omp atomic
 		histogram[i]++;
 	}
-	cout << "here " << endl;
-// }
 
 	long maxID = 0;
 	long minID = 0;
@@ -164,13 +132,8 @@ Result * Experiment::calc(long experiments) {
 	double sum = 0.0;
 	long values = 0;
 
-	cout << "vars " << endl;
-
-// for shared(maxN, maxID, hmin, hmax, histogram, sum) private(idx)\
-				// reduction(+ : sum, values)
 #pragma omp parallel for
 	for (long idx = hmin; idx <= hmax; idx++) {
-		// cout << "_-________------______------_____-----______-----_______------_________-------------" << endl;
 		if (maxN < histogram[idx]) {
 			maxN = histogram[idx];
 			maxID = idx;
